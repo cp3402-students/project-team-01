beec9c504821c1da9f6dfb072da1f06272388925
VanessaCalimag <vanessa.calimag@my.jcu.edu.au>
2023-05-19 06:19:58 +1000

    Registed new sidebars for Facebook & Magazine

--- functions.php
+++ functions.php
@@ -9,5 +9,30 @@ function tt_add_styles() {
         array( 'parent-style'), 
         wp_get_theme()->get('Version')
     );
-    wp_enqueue_style( 'underscores-child-theme-fonts', "https://fonts.googleapis.com/css2?family=Alegreya&family=Alegreya+Sans&display=swap");
 }
+
+function underscores_child_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Facebook Sidebar', 'underscores' ),
+			'id'            => 'facebook_bar',
+			'description'   => esc_html__( 'Add widgets here.', 'underscores' ),
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+    register_sidebar(
+		array(
+			'name'          => esc_html__( 'Magazine Sidebar', 'underscores' ),
+			'id'            => 'archive_bar',
+			'description'   => esc_html__( 'Add widgets here.', 'underscores' ),
+			'before_widget' => '<li id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</li>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
+		)
+	);
+}
+add_action( 'widgets_init', 'underscores_child_widgets_init' );
